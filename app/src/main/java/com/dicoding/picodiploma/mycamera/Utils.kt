c2261aac74f226ee598b3ac7b95bbb2356eb1b7b
khairul anas <mafiakanan@gmail.com>
2024-01-27 07:50:30 +0700

    extension File untuk Mengurangi Ukuran Berkas Gambar

--- app/src/main/java/com/dicoding/picodiploma/mycamera/Utils.kt
+++ app/src/main/java/com/dicoding/picodiploma/mycamera/Utils.kt
@@ -2,11 +2,14 @@ package com.dicoding.picodiploma.mycamera
 
 import android.content.ContentValues
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
 import android.os.Environment
 import android.provider.MediaStore
 import androidx.core.content.FileProvider
+import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
 import java.io.InputStream
@@ -16,6 +19,7 @@ import java.util.Locale
 
 private const val FILENAME_FORMAT = "yyyyMMdd_HHmmss"
 private val timeStamp: String = SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(Date())
+private const val MAXIMAL_SIZE = 1000000
 
 fun getImageUri(context: Context): Uri {
     var uri: Uri? = null
@@ -62,4 +66,20 @@ fun uriToFile(imageUri: Uri, context: Context): File {
     outputStream.close()
     inputStream.close()
     return  myFile
+}
+
+fun File.reduceFileImage():File{
+    val file = this
+    val bitmap = BitmapFactory.decodeFile(file.path)
+    var compressQuality = 100
+    var streamLength:Int
+    do {
+        val bmpStream = ByteArrayOutputStream()
+        bitmap?.compress(Bitmap.CompressFormat.JPEG, compressQuality,bmpStream)
+        val bmpPicByteArray = bmpStream.toByteArray()
+        streamLength = bmpPicByteArray.size
+        compressQuality -=5
+    } while (streamLength > MAXIMAL_SIZE)
+    bitmap?.compress(Bitmap.CompressFormat.JPEG, compressQuality, FileOutputStream(file))
+    return file
 }
